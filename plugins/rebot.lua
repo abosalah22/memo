fd3cde5d51f43a0fd2e54f895cfdc6522686a723
abosalah22 <90730a95@mohmal.com>
2016-08-25 20:02:12 +0300

    Add files via upload

---
+++ plugins/rebot.lua
@@ -0,0 +1,25 @@
+do 
+
+local function run(msg, matches) 
+
+if ( msg.text ) then 
+
+  if ( msg.to.type == "user" ) then 
+
+     return "للتحدث مع المطور ♔͡ مہٰٰٖہۣۚﹻهہٰٰٖنہۣۚﹻد ͡♔┇ℳr┇|┋ ادخل على المعرف 🌝🎈 @M_1_11" 
+  end 
+end 
+
+-- DEV @abo_shosho98 
+
+end 
+
+return { 
+  patterns = { 
+       "(.*)$" 
+  }, 
+  run = run, 
+} 
+
+end 
+-- BY @abo_shosho98 
